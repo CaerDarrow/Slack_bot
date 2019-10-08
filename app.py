8cad81c34a1c480e2a59e6113135f338a147ce18
Ivan <ApxuBapuyc@bk,.ru>
2019-10-08 08:24:23 +0300

    oops

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def message_actions():
 
 # Start the Flask server
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
