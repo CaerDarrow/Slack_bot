b3b96f87c9fcb5f59229fde236d4605acfbabbdd
ajon-hol <ApxuBapuyc@bk.ru>
2019-11-07 22:03:28 +0300

    Add book get from api

--- block_kit.py
+++ block_kit.py
@@ -180,13 +180,13 @@ class BlockKit:
     def get_more_books(self, action_id, action_value, blocks, team_id):
         true_action = action_id.split('-')[1]
         selector, start = action_value.split('-')
-        blocks = [self.get_book_list(true_action, selector, team_id, int(start))
-                  if "text" in section.keys() and section["text"]["text"][1:] == selector
-                  else section for section in blocks if
+        blocks = [section for section in blocks if
                   "elements" in section.keys() and
                   section["elements"][0]["value"] != action_value
-                  or "elements" not in section.keys()
-        ]
+                  or "elements" not in section.keys()]
+        blocks = [self.get_book_list(true_action, selector, team_id, int(start))
+                  if "text" in section.keys() and section["text"]["text"][1:] == selector
+                  else section for section in blocks ]
         return blocks
 
     def get_return_book_block(self, book_id):
